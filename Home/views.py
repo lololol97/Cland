1d1587f1beb0a8fe0e5ee3368bfc478b4dc55e38
lololol97 <sriragav.naresh@gmail.com>
2021-07-22 13:29:03 -0400

    Hi 12 year old gogogaga

--- Home/views.py
+++ Home/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def initial(request):
+    return HttpResponse("<h1> Hi Rishit, \n if you are seeing this then I decided we need to make a fresh new website because i have imporved my django skills and JavaScript skills  </h1>")
